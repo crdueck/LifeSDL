c5803e3f3864251b5a3916c7b290020e7614c800
Chris Dueck <cdueck93@hotmail.com>
2012-10-12 21:03:01 -0400

    added comments, added saving/loading game states

---
+++ Life.hs
@@ -0,0 +1,40 @@
+module Life where
+import Data.List (group, sort, concatMap)
+import Control.Arrow ((&&&))
+
+-- test input
+fPent :: Board
+fPent = [(1, 2), (2, 2), (2, 1), (2, 3), (3, 3)]
+
+-- some simple type wrappers
+type Edge  = (Int, Int)
+type Cell  = (Int, Int)
+type Board = [Cell]
+
+-- life: progresses the current game state one turn according to the rules of Life.
+-- Uses neighbours to create a list containing all Cells which
+-- neighbour a live Cell, and counts the number of occurences of each Cell.
+-- The number of occurrences of a Cell in this list is the number of live
+-- Cells neighbouring it, and the Cells which will not progress to the next
+-- stage of Life can then be filtered out.
+life :: Board -> Board
+life cells = map snd . filter rules . freq $ concatMap neighbours cells
+    where rules (n, c) = n == 3 || (n == 2 && c `elem` cells)
+          freq = map (length &&& head) . group . sort
+
+-- neighbours: creates a list containing all of the neighbours of a given
+-- Cell, excluding the Cell itself.
+neighbours :: Cell -> [Cell]
+neighbours (x, y) = [(x + dx, y + dy) | dx <- [-1..1], dy <- [-1..1], dx /= 0 || dy /= 0]
+
+-- "wrap" a Cell around the edges of a Board, to simulate an infinite playing surface
+wrapCell :: Edge -> Cell -> Cell
+wrapCell (w, h) (x, y) = (wrap x w, wrap y h)
+    where wrap a b
+            | a < 0     = b + a
+            | a > b     = a - b
+            | otherwise = a
+
+-- apply wrapCell to every Cell in a Board
+wrapBoard :: Edge -> Board -> Board
+wrapBoard edges = map (wrapCell edges)
--- LifeSDL.hs
+++ LifeSDL.hs
@@ -1,37 +1,47 @@
-import LifeV
+import Life
 import Control.Monad
 import Graphics.UI.SDL as SDL
 import Data.List (nub)
-import qualified Data.Vector as V
+
+-- global config options
 
 cellsize = 4
+height   = 480
+width    = 640
+depth    = 32
 
+-- wrap a Cell in the SDL.Rect data type
 cellToRect :: Cell -> Rect
 cellToRect (x, y) = Rect (cellsize * x) (cellsize * y) cellsize cellsize
 
+-- add a Cell to the screen surface, but dont draw it yet
 addCell :: Surface -> Cell -> IO Bool
 addCell screen cell = do
     pixel   <- mapRGB (surfaceGetPixelFormat screen) 255 255 255
     fillRect screen (Just $ cellToRect cell) pixel
 
+-- draw a Board to the screen surface
 drawBoard :: Surface -> Board -> IO ()
 drawBoard screen b = do
     pixel   <- mapRGB (surfaceGetPixelFormat screen) 0 0 0
     fillRect screen Nothing pixel
-    V.mapM_ (addCell screen) b
+    mapM_ (addCell screen) b
     SDL.flip screen
 
+-- draw a cursor (grey) to the screen surface
 drawCursor :: Surface -> Cell -> IO ()
 drawCursor screen cell = do
     pixel   <- mapRGB (surfaceGetPixelFormat screen) 125 125 125
     fillRect screen (Just $ cellToRect cell) pixel
     SDL.flip screen
 
+-- poll for events from the user, like quitting or saving the game state.
+-- if no user events occur, continually draws and updates the game state.
 gen :: Edge -> Surface -> Board -> IO ()
-gen edge screen board = do
+gen edges screen board = do
     quit    <- whileEvents
-    drawBoard screen board
-    unless quit (gen edge screen $ life board)
+    drawBoard screen $ wrapBoard edges board
+    unless quit (gen edges screen $ life board)
     where whileEvents = do
               event     <- pollEvent
               case event of
@@ -42,63 +52,70 @@ gen edge screen board = do
                             _       -> whileEvents
                    _   -> return False
 
-userSeed :: Surface -> [Cell] -> Cell -> IO Board
+-- create an initial game state from user input
+userSeed :: Surface -> Board -> Cell -> IO Board
 userSeed screen seed pos@(x, y) = do
     event   <- pollEvent
     case event of
          KeyDown (Keysym key _ _) -> do
              case key of
                   SDLK_UP       -> do
-                      drawCursor screen (x, y - 1)
-                      userSeed screen seed (x, y - 1)
+                      updateCursor (x, y - 1)
                   SDLK_DOWN     -> do
-                      drawCursor screen (x, y + 1)
-                      userSeed screen seed (x, y + 1)
+                      updateCursor (x, y + 1)
                   SDLK_LEFT     -> do
-                      drawCursor screen (x - 1, y)
-                      userSeed screen seed (x - 1, y)
+                      updateCursor (x - 1, y)
                   SDLK_RIGHT    -> do
-                      drawCursor screen (x + 1, y)
-                      userSeed screen seed (x + 1, y)
+                      updateCursor (x + 1, y)
                   SDLK_RETURN   -> do
+                      drawBoard screen seed
                       addCell screen pos
                       SDL.flip screen
                       userSeed screen (pos : seed) pos
-                  SDLK_d        -> return $ V.fromList $ nub seed
+                  SDLK_b        -> return $ nub seed
                   _             -> userSeed screen seed pos
          _  -> userSeed screen seed pos
+         where updateCursor (x, y) = do
+                   drawBoard screen seed
+                   drawCursor screen (x, y)
+                   userSeed screen seed (x, y)
 
+-- save the current game state to file as a plain-text Haskell list
 saveBoard :: Board -> IO Bool
 saveBoard b = do
     putStrLn "Where should the game state be saved to?"
     name    <- getLine
     writeFile name $ show b
-    putStrLn $ "Finished saving to " ++ name
+    putStrLn $ "Finished saving to " ++ name ++ "."
     putStrLn "Continue playing?"
     cont    <- getLine
     case cont of
          "y"    -> return False
          _      -> return True
 
+-- load a game state from a save file
 loadBoard :: IO (Maybe Board)
 loadBoard = do
-    putStrLn "Load a game?"
+    putStrLn "Load a game? (y/n)"
     yn  <- getLine
     case yn of
          "y"    -> do
-             putStrLn "what is the name of the file?"
+             putStrLn "What is the name of the file?"
              contents   <- getLine >>= readFile
              return $ Just $ read contents
          _      -> return Nothing
 
 main = withInit [InitEverything] $ do
     screen  <- setVideoMode width height depth [SWSurface]
-    {-seed    <- userSeed screen [] origin-}
-    gen edges screen $ V.map (translate origin) fPent
-    where makeEdge x = floor $ (fromIntegral x) / (fromIntegral cellsize)
-          edges = (makeEdge width, makeEdge height)
-          translate (l, u) (x, y) = (x + l, y + u)
-          height = 480
-          width  = 640
-          depth  = 32
-          origin = (60, 50)
+    load    <- loadBoard
+    let load' = case load of
+                    Nothing -> []
+                    Just xs -> xs
+    drawBoard screen load'
+    seed    <- userSeed screen load' centre
+    putStrLn "Press 'q' to quit.\nPress 's' to save."
+    gen edges screen seed
+    where intDiv n d = floor $ (fromIntegral n) / (fromIntegral d)
+          centre     = (intDiv width (2 * cellsize), intDiv height (2 * cellsize))
+          makeEdge   = Prelude.flip intDiv cellsize
+          edges      = (makeEdge width, makeEdge height)
